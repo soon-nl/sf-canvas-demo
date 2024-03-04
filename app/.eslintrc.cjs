3cca0dcd61588e533d903d8fa4434e348b932ce4
Soon Hong Ooi <soon.hongooi@slalom.com>
2024-03-04 15:33:25 +0000

    Added ESLint

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'airbnb',
+		'airbnb/hooks',
+		'airbnb-typescript',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:prettier/recommended',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json'],
+	},
+	plugins: ['@typescript-eslint', 'react'],
+	rules: {
+		'react/react-in-jsx-scope': 0,
+		// Configure prettier
+		'prettier/prettier': [
+			'error',
+			{
+				printWidth: 80,
+				endOfLine: 'lf',
+				singleQuote: true,
+				tabWidth: 2,
+				indentStyle: 'space',
+				useTabs: true,
+				trailingComma: 'es5',
+			},
+		],
+	},
+	ignorePatterns: ['vite.config.ts'],
+};
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,64 +1,80 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '../../../../../../vite.svg';
+import './App.css';
 
 function App() {
-  const [sfContext, setSFContext] = useState<Record<string, unknown>>();
+	const [sfContext, setSFContext] = useState<Record<string, unknown>>();
 
+	const [response, setResponse] = useState<Record<string, unknown>>();
 
-  const [response, setResponse] = useState<Record<string, unknown>>();
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		setSFContext(window.sfContext);
+	}, []);
 
-  useEffect(() => {
-  // @ts-ignore
-   setSFContext(window.sfContext);
-  }
-  , []);
+	const consumeEndpoint = async () => {
+		try {
+			const requestOptions: RequestInit = {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+			};
 
+			const dataResponse = await fetch(
+				'https://baintest.azurewebsites.net/api/HttpDataCalculation?code=eLxoDH_Sk0zmipfOM0uwxpL-lk8FwYxQr89h8xXEj0L3AzFuFEfywQ==',
+				requestOptions
+			);
+			const data: Record<string, unknown> = await dataResponse.json();
+			setResponse(data);
 
-  const consumeEndpoint = async ()=> {
-    try {
-      const requestOptions: RequestInit = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-      };
+			console.log(data);
+		} catch (error) {
+			console.error('Error fetching data:', error);
+			// Handle error state or display error message to the user
+		}
+	};
 
-      const response = await fetch('https://baintest.azurewebsites.net/api/HttpDataCalculation?code=eLxoDH_Sk0zmipfOM0uwxpL-lk8FwYxQr89h8xXEj0L3AzFuFEfywQ==', requestOptions);
-      const data: Record<string, unknown> = await response.json();
-      setResponse(data);
-
-      console.log(data);
-      
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      // Handle error state or display error message to the user
-      
-    }
-  };
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <button onClick={consumeEndpoint}>RUN ME</button>
-      {response && (
-        <div>
-          <p>Message:</p>
-          {/* Render other data from the response as needed */}
-          <textarea name="response" rows={10} cols={60} value={JSON.stringify(response)} readOnly />
-        </div>
-      )}
-      {sfContext && (<div><p>SFContext:</p>
-      <textarea name="response" rows={10} cols={60} value={JSON.stringify(sfContext)} readOnly /></div>)}
-    </>
-  )
+	return (
+		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<button type="button" onClick={consumeEndpoint}>
+				RUN ME
+			</button>
+			{response && (
+				<div>
+					<p>Message:</p>
+					{/* Render other data from the response as needed */}
+					<textarea
+						name="response"
+						rows={10}
+						cols={60}
+						value={JSON.stringify(response)}
+						readOnly
+					/>
+				</div>
+			)}
+			{sfContext && (
+				<div>
+					<p>SFContext:</p>
+					<textarea
+						name="response"
+						rows={10}
+						cols={60}
+						value={JSON.stringify(sfContext)}
+						readOnly
+					/>
+				</div>
+			)}
+		</>
+	);
 }
 
-export default App
+export default App;
